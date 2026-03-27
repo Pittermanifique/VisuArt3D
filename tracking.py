af48cb09e091858c3974e562b3903bf7a155103d
usersi <usersi@si.si.com>
2026-03-27 16:15:39 +0100

    midiapie fix crea esp32dir

--- tracking.py
+++ tracking.py
@@ -13,7 +13,7 @@ def face_detection(buffer_track,queue = None,camera_index=0):
     face_cascade = cv2.CascadeClassifier(face_cascade_path)
 
     base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
-    options = vision.HandLandmarkerOptions(base_options=base_options,num_hands=1)
+    options = vision.HandLandmarkerOptions(base_options=base_options,num_hands=16)
     detector = vision.HandLandmarker.create_from_options(options)
 
     life_pouce = 0
